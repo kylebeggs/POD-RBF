2693ffae5a7dc373a002d93a11c16dceee197f14
Kyle Beggs <beggskw@gmail.com>
2023-08-17 09:23:15 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pod_rbf",  # Replace with your own username
-    version="1.3.0",
+    version="{{VERSION_PLACEHOLDER}}",
     author="Kyle Beggs",
     author_email="beggskw@gmail.com",
     description="Tool to perform interpolation using the Proper Orthogonal Decomposition - Radial Basis Function (POD-RBF) method.",
